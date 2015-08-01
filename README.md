c081af79cf74f73a13e660dd8ad3493a56c21e36
ross-t <ross.s.trundy@gmail.com>
2015-08-01 13:44:21 -0400

    Updated README.md, finished up reference binary heap

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 #python-ds
 A collection of sample data structures and algorithms implemented in Python. The intention isn't that you should use these in production, it's for you to learn from them (and trust me, they're not perfect; many are intentionally simplified to make them more easily understandable, even when it sacrifices proper details and makes performance worse)
 
+
+#Graphs
 [graphpaths.py](Graphs/graphs.py) requires numpy. It's an implementation of Floyd-Warshall and Kruskal's algorithms. You can use it to generate random simple graphs and it has the (limited) ability to create files readable by graphviz, but only under some circumstances (and it desperately needs cleaning up). If you actually need to work with graphs in Python, I recommend using [networkx](https://networkx.github.io/).
 
 There's a disjoint set in there, too.
 
 [simplegraph.py](Graphs/simplegraph.py) is a basic adjacency-list graph implementation with reference implementations of depth and breadth-first search. It also demonstrates how to find components of a graph.
 
-[hashtable.py](Tables/hashtable.py) is a simple hash table implementation.
+#Matrices
+[MatrixChainMultiplication.py](Matrices/MatrixChainMultiplication.py) is a dynamic programming solution to the matrix chain multiplication problem. It requires numpy.
 
-[splay_tree.py](Trees/splay_tree.py) implements a key-value storage splay tree.
-
+#Sorting
 [mergesort.py](Sorting/mergesort.py) is mergesort. Surprise!
 
 [quicksort.py](Sorting/quicksort.py) is quicksort. Surprise again!
 
-[MatrixChainMultiplication.py](Matrices/MatrixChainMultiplication.py) is a dynamic programming solution to the matrix chain multiplication problem. It requires numpy.
+#Tables
+[hashtable.py](Tables/hashtable.py) is a simple hash table implementation.
+
+#Trees
+[splay_tree.py](Trees/splay_tree.py) implements a key-value storage splay tree.
+[binaryheap.py](Trees/binaryheap.py) is a min-heap implemented with array storage.
--- Trees/binaryheap.py
+++ Trees/binaryheap.py
@@ -18,6 +18,7 @@ class ExampleBinaryHeap(object):
 			self.up_heap(parent_index)
 
 	def delete_min(self):
+		'''Remove the smallest item from the heap, then down-heap as necessary'''
 		res = self.heap[0]
 		self.heap[0] = self.heap[-1]
 		self.heap = self.heap[:-1]
@@ -25,6 +26,7 @@ class ExampleBinaryHeap(object):
 		return res
 
 	def down_heap(self, item_index):
+		'''Recursively bubble an item down the heap until we don't violate the heap property'''
 		children = get_children(self.heap, item_index)
 		if children[0] == None and children[1] == None:
 			return
@@ -49,24 +51,36 @@ class ExampleBinaryHeap(object):
 			self.down_heap(child_indices[1])
 
 	def find_min(self):
+		'''Return the smallest item in the heap (the root, as this is a min-heap)'''
 		return self.heap[0]
 
 	def is_empty(self):
+		'''Return whether the heap is empty or not'''
 		return len(self.heap) == 0
 
 	def size(self):
+		'''Return how many elements are in the heap'''
 		return len(self.heap)
 
 	@staticmethod
-	def build_heap(list):
-		pass
+	def build_heap(heap_list):
+		'''Build a heap from the elements in the list, then return it'''
+		new_heap = ExampleBinaryHeap()
+		for x in heap_list:
+			new_heap.insert(x)
+		return new_heap
+
+	def __contains__(self, item):
+		return item in self.heap
 
 def swap(heap, first_index, second_index):
+	'''Swap the items at first_index and second_index in the heap'''
 	temp = heap[first_index]
 	heap[first_index] = heap[second_index]
 	heap[second_index] = temp
 
 def get_children(heap, parent_index):
+	'''Return a tuple of (left child, right child) of the node at index parent_index'''
 	if (2 * parent_index + 1) >= len(heap):
 		return (None, None)
 	if (2 * parent_index + 2) >= len(heap):
@@ -74,25 +88,18 @@ def get_children(heap, parent_index):
 	return (heap[2 * parent_index + 1], heap[2 * parent_index + 2])
 
 def get_parent(heap, child_index):
+	'''Return the value of the parent of the node at child_index, or None if that node is the root'''
 	parent_index = get_parent_index(child_index)
 	if parent_index == None:
 		return None
 	return heap[parent_index]
 
 def get_parent_index(child_index):
+	'''Return the index of the parent of the node at child_index, or None if that node is the root'''
 	if child_index == 0:
 		return None
 	return (child_index - 1) // 2
 
 def get_child_indices(parent_index):
+	'''Return a tuple of (left child index, right child index) of the node at parent_index'''
 	return ((2 * parent_index + 1), (2 * parent_index + 2))
-
-if __name__ == '__main__':
-	e = ExampleBinaryHeap()
-	e.insert(11)
-	e.insert(5)
-	e.insert(8)
-	e.insert(3)
-	e.insert(4)
-	e.delete_min()
-	print e.heap
